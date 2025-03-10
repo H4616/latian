37f776edc4fcb5c8657dd82711e6e909591edead
H4616 <103203538+H4616@users.noreply.github.com>
2025-03-10 23:58:12 +0700

    buang ga penting

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart'; // Import font_awesome_flutter
+
 
 class LoginPages extends StatefulWidget {
   const LoginPages({super.key});
